744a2d02973e8322218f6f089ca59df1093c51f9
nehru <nehruyadav729@gmail.com>
2022-05-29 17:08:39 +0530

    changed

---
+++ js1.js
@@ -0,0 +1,20 @@
+/*const a = 1;
+const b = 2;
+const boolval = true;
+const str = "nehru yadav"
+for(i=0;i<10;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+    else{
+        console.log("not prime");
+    }
+}
+console.log(a,b,boolval,str);*/
+
+/*const firstName = "nehru";
+const lastName = "yadav";
+console.log(firstName + lastName)*/
+const t = 10;
+const t1 = 12;
+console.log(t+t1,t*t1,t/t1)
